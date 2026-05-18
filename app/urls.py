f165c1cf0fe73bd1c9b0420aee63f45b28ae6d57
anshukumarrr <anshupersonal2471@gmail.com>
2026-05-19 01:18:23 +0530

    till dashboard

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns=[
-    path('', views.landing_view, name='landing_page')
+    path('', views.landing_view, name='landing_page'),
+    path('register',views.RegisterAPIView.as_view(), name='register'),
+    path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('dashboard/',views.DashboardAPIView.as_view(),name='dashboard'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,11 +5,78 @@ from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView
+from .serializers import RegisterSerializer
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
 
 def landing_view(request):
     return render(request,'landing.html',) 
 
-def signup_view(request):
-    if request.method =="POST":
-        form=SignupForm(request.POST)
+class RegisterAPIView(APIView):
+    def get(self,request):
+        return render(request,'register.html')
+    def post(self, request):
+
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response(
+                {
+                    "message": "User created successfully"
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+class LoginAPIView(APIView):
+        def get(self,request):
+             return render(request,'login.html')
+        def post(self, request):
+
+            username = request.data.get('username')
+            password = request.data.get('password')
+
+            user = authenticate(
+                username=username,
+                password=password
+            )
+
+            if user is not None:
+                login(request,user)
+                refresh = RefreshToken.for_user(user)
+                return redirect('dashboard')
+
+                
+                
+                return Response(
+                    
+                    {
+                        'message': 'Login successful',
+
+                        'refresh': str(refresh),
+
+                        'access': str(refresh.access_token),
+                    },
+                    status=status.HTTP_200_OK
+                    
+                )
+
+            return Response(
+                {
+                    'error': 'Invalid credentials'
+                },
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+class DashboardAPIView(APIView):
+     def get(self,request):
+          return render(request,'dashboard.html')
